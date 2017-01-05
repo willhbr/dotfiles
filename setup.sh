2b5787f86a62310bee998f3bc6c30dc314986464
Will Richardson <william.hamish@gmail.com>
2017-01-05 19:46:24 +1100

    Override PWD persistence on tmux 1.8

--- setup.sh
+++ setup.sh
@@ -39,5 +39,19 @@ link ptpython/config.py ~/.ptpython/config.py
 # Tmux
 link tmux/tmux.conf ~/.tmux.conf
 
+if $(which tmux) && [ `tmux -V` = *1.8 ]; then
+
+cat << EOF > ~/.tmux.conf
+# OVERRIDE FOR TMUX 1.8
+unbind c
+unbind h
+unbind v
+bind c new-window"
+bind v split-window -h"
+bind h split-window -v"
+EOF
+
+fi
+
 # Extra commands
 link bin ~/.wills-bin
