39f8ca3a168ce3a3ac60bd9d915b502641a848b7
Will Richardson <william.hamish@gmail.com>
2017-01-14 21:39:55 +1300

    Added Julius's tmux completion, better bash prompt

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ current=$(pwd)
 function link() {
   path="$current/$1"
   mkdir -p $(dirname $2)
-  if [ ! -d $2 ]; then
+  if [ -L $2 ]; then
+    rm $2
     echo "$2 -> $path"
     ln -fs $path $2
   fi
@@ -40,20 +41,6 @@ link ptpython/config.py ~/.ptpython/config.py
 
 # Tmux
 link tmux/tmux.conf ~/.tmux.conf
-
-if $(which tmux) && [ `tmux -V` = *1.8 ]; then
-
-cat << EOF > ~/.tmux.conf
-# OVERRIDE FOR TMUX 1.8
-unbind c
-unbind h
-unbind v
-bind c new-window"
-bind v split-window -h"
-bind h split-window -v"
-EOF
-
-fi
 
 # Extra commands
 link bin ~/.wills-bin
