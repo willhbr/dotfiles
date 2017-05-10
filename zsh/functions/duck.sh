122465acc1f0f310cc67667f4daef08a91886bc6
Will Richardson <william.hamish@gmail.com>
2017-05-10 22:37:28 +1200

    Added colours to duck command and stopped highlighting from being reloaded

--- zsh/functions/duck.sh
+++ zsh/functions/duck.sh
@@ -3,10 +3,17 @@
 duck() {
   message="$1"
   [ -z "$message" ] && message="Duck!"
-cat <<- EOF
-     ___
- ___( o )>  $(echo "$message" | xargs)
+  local text=""
+  local yellow="\033[38;5;220m"
+  local orange="\033[38;5;202m"
+  local green="\033[38;5;83m"
+  local blue="\033[38;5;33m"
+read -r -d '' text <<- EOF
+\033[38;5;220m     ___
+ ___( o )$orange>  $green$(echo "$message" | xargs) $yellow
  \ <_. )
-  \`---'
+$blue~~$yellow\`---'$blue~~~~~~~~
+@@@@@@@@@@@@@@@\033[0m
 EOF
+  echo -e "$text"
 }
