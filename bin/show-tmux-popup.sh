973090bcc13a343b84e4c1b80ced13d843ba352c
Will Richardson <william.hamish@gmail.com>
2023-02-08 12:34:54 +0000

    Update git stuff and show popup in status line

--- bin/show-tmux-popup.sh
+++ bin/show-tmux-popup.sh
@@ -4,7 +4,7 @@ session="$(tmux display -p '_popup_#S')"
 
 if ! tmux has -t "$session" 2> /dev/null; then
   parent_session="$(tmux display -p '#{session_id}')"
-  session_id="$(tmux new-session -dP -s "$session" -F '#{session_id}' -e TMUX_PARENT_SESSION="$parent_session")"
+  session_id="$(tmux new-session -c '#{pane_current_path}' -dP -s "$session" -F '#{session_id}' -e TMUX_PARENT_SESSION="$parent_session")"
   tmux set-option -s -t "$session_id" key-table popup
   tmux set-option -s -t "$session_id" status off
   tmux set-option -s -t "$session_id" prefix None
