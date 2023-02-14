d4c0d02a58f9411083377ada634352669da2ccb5
Will Richardson <william.hamish@gmail.com>
2023-02-14 09:40:21 +0000

    Optimise ssh thingie

--- shell/ssh.sh
+++ shell/ssh.sh
@@ -2,7 +2,11 @@
 
 # Print the hostname when exiting from an ssh connection
 ssh() {
-  if [ -n "$TMUX" ]; then
+  local do_tmux=''
+  if [ -n "$TMUX" ] && [ "$(tmux list-panes -F 'P')" = 'P' ]; then
+    do_tmux='yeah boy'
+  fi
+  if [ -n "$do_tmux" ]; then
     local prefix="$(tmux display -p '#{prefix}')"
     local status_mode="$(tmux display -p '#{status}')"
     tmux set status off
@@ -13,7 +17,7 @@ ssh() {
   local res=$?
   echo
   echo -e "Back on \033[38;5;${HOST_COLOR}m$(hostname -s)\033[0m as $(whoami)"
-  if [ -n "$TMUX" ]; then
+  if [ -n "$do_tmux" ]; then
     tmux set status "$status_mode"
     tmux set key-table root
     tmux set prefix "$prefix"
