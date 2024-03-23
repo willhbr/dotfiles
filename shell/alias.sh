fae7f784ab4befe9fc25de690e379810194671f4
Will Richardson <william.hamish@gmail.com>
2024-03-23 00:48:16 +0000

    Simplify aliases

--- shell/alias.sh
+++ shell/alias.sh
@@ -14,28 +14,13 @@ alias py="python3"
 alias :q="exit"
 
 # Git
-chpwd() {
-  if [ -e .jj ]; then
-    alias git=jj
-    return
-  fi
-  if [ -e .git ]; then
-    alias git=/usr/bin/git
-    return
-  fi
-}
-chpwd
-alias g="git"
-alias gs=" git status"
-alias gd=" git diff"
-alias gcm=" git commit -m"
-alias gc=" git commit"
-alias gl=" git log"
-alias gch=" git checkout"
-alias gb=" git branch"
-alias gaa=" git add --all"
-alias gap=" git add --patch"
-alias gp=" git push"
+alias g="jj"
+alias gs=" g status"
+alias gd=" g diff"
+alias gcm=" g commit -m"
+alias gc=" g commit"
+alias gl=" g log"
+alias gp=" g push"
 
 alias docker=podman
 
